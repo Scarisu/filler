4dd74c02df7dedcae57b5dda2e9cc257df1b3d64
Pacome BERNIER <pbernier@e2r12p1.42.fr>
2017-08-18 15:21:39 +0200

    [pbernier] - save

--- includes/filler.h
+++ includes/filler.h
@@ -6,11 +6,17 @@
 /*   By: pbernier <pbernier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/18 14:39:22 by pbernier          #+#    #+#             */
-/*   Updated: 2017/08/18 14:59:34 by pbernier         ###   ########.fr       */
+/*   Updated: 2017/08/18 15:21:02 by pbernier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLER_H
 # define FILLER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
 
 #endif
